101386975255ca8752e929f2cc0ec3b970ed0122
Marcus Armstrong <marcusdarmstrong@gmail.com>
2022-06-26 17:02:09 -0400

    Null checking

--- mdns-publisher/mdns-publisher.js
+++ mdns-publisher/mdns-publisher.js
@@ -8,11 +8,14 @@ for await (const ping of setInterval(30_000)) {
   if (response.ok) {
   	const routers = await response.json();
   	const hosts = new Set(routers.map(({ rule }) => {
-  	  const [ match, host, ...rest ] = rule.match(/^Host\(`([a-z0-9_-]+local)`\)$/)
-  	  if (match && !(host in registrations)) {
-  	  	const aborter = new AbortController();
-  	  	exec(`avahi-publish -R -a homebridge.home.local ${process.env.BINDING_IP_ADDRESS}`, { signal: aborter.signal });
-  	  	registrations[host] = aborter.abort;
+  	  const match = rule.match(/^Host\(`([a-z0-9_-]+local)`\)$/)
+  	  if (match) {
+		const [ full, host, ...rest ] = match;
+  	  	if (!(host in registrations)) {
+  	  	  const aborter = new AbortController();
+  	  	  exec(`avahi-publish -R -a homebridge.home.local ${process.env.BINDING_IP_ADDRESS}`, { signal: aborter.signal });
+  	  	  registrations[host] = aborter.abort;
+  	    }	
   	  }
   	}));
   	Object.entries(registrations).forEach(([host, abort]) => {
