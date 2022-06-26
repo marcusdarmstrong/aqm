0705ba0115fc19c6097ca268085f743576e3c5d0
Marcus Armstrong <marcusdarmstrong@gmail.com>
2022-06-26 15:17:31 -0400

    Trying my own code

---
+++ mdns-publisher/mdns-publisher.js
@@ -0,0 +1,25 @@
+import { exec } from 'node:child_process';
+import { setInterval } from 'node:timers/promises';
+
+const registrations = {};
+
+for await (const ping of setInterval(30_000)) {
+  const response = await fetch(`${process.env.TRAEFIK_API_HOST}/api/http/routers`)
+  if (response.ok) {
+  	const routers = await response.json();
+  	const hosts = new Set(routers.map(({ rule }) => {
+  	  const [ match, host, ...rest ] = rule.match(/^Host\(`([a-z0-9_-]+local)`\)$/)
+  	  if (match && !(host in registrations)) {
+  	  	const aborter = new AbortController();
+  	  	exec(`avahi-publish -R -a homebridge.home.local ${process.env.BINDING_IP_ADDRESS}`, { signal: aborter.signal });
+  	  	registrations[host] = aborter.abort;
+  	  }
+  	}));
+  	Object.entries(registrations).forEach(([host, abort]) => {
+  	  if (!hosts.has(host)) {
+  	  	abort();
+  	  	delete registrations[host];
+  	  }
+  	});
+  }
+}
