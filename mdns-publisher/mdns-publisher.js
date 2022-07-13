b5cd72d4e5822274ae1c4fda40b55bdb0986a81d
Marcus Armstrong <marcusdarmstrong@gmail.com>
2022-07-12 21:44:13 -0400

    Fix my spacing

--- mdns-publisher/mdns-publisher.js
+++ mdns-publisher/mdns-publisher.js
@@ -5,35 +5,35 @@ const registrations = {};
 const loop = async () => {
   const response = await fetch(`${process.env.TRAEFIK_API_HOST}/api/http/routers`)
   if (response.ok) {
-  	const routers = await response.json();
-  	const hosts = new Set()
-  	routers.forEach(({ rule }) => {
+    const routers = await response.json();
+    const hosts = new Set()
+    routers.forEach(({ rule }) => {
       const hostRule = rule.match(/Host\(([a-z0-9_\.`, \t-]+)\)/);
       if (hostRule === null) {
         return;
       }
       const [ _, hostSet, ...rest ] = hostRule;
       for (const match of hostSet.matchAll(/`([a-z0-9\._-]+\.local)`/g)) {
-		    const [ m, host ] = match;
-		    hosts.add(host);
-  	  	if (!(host in registrations)) {
-  	  	  const aborter = new AbortController();
-  	  	  spawn(
+        const [ m, host ] = match;
+        hosts.add(host);
+        if (!(host in registrations)) {
+          const aborter = new AbortController();
+          spawn(
             'avahi-publish',
             ['-R', '-a', `${host}`, `${process.env.BINDING_IP_ADDRESS}`],
             { signal: aborter.signal, stdio: 'inherit' },
           );
-  	  	  registrations[host] = () => aborter.abort();
-  	      console.log(`Published ${host}`);
-  	    }
+          registrations[host] = () => aborter.abort();
+          console.log(`Published ${host}`);
+        }
       }
-  	});
-  	Object.entries(registrations).forEach(([host, abort]) => {
-  	  if (!hosts.has(host)) {
-  	  	abort();
-  	  	delete registrations[host];
-  	  }
-  	});
+    });
+    Object.entries(registrations).forEach(([host, abort]) => {
+      if (!hosts.has(host)) {
+        abort();
+        delete registrations[host];
+      }
+    });
   }
 }
 
